c70b71c23503328310d421fad66dc56f11064397
keirbailey <keirinvestment@gmail.com>
2020-09-19 13:07:17 +0100

    Initial project structure

---
+++ src/main/java/finance/modelling/data/ingestfinancialexchangetickers/IngestFinancialExchangeTickersApplication.java
@@ -0,0 +1,13 @@
+package finance.modelling.data.ingestfinancialexchangetickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestFinancialExchangeTickersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestFinancialExchangeTickersApplication.class, args);
+	}
+
+}
